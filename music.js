e7d66ea9cedda53baa2182fd89d126c4e1c74efe
panah <amirmahdi.code@gmail.com>
2026-09-07 11:33:40 +0330

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-const MUSIC_API = 'https://script.google.com/macros/s/AKfycbyW1IvZYTXVsD9-D_j2O9J_jU0qdUPcVcvQrjpqyQinXRF31VvWDkOhHiwrPME1Hs6G/exec';
+const MUSIC_API = 'https://script.google.com/macros/s/AKfycbypWWml5JQ0CIjurUfrOVRkI5O33d2u3GT-51hzEliYa1JZ8_3oA9Mfh8gNeCDhIvRS/exec';
 const MUSIC_KEY = 'heyat_music_state';
 
 class MusicPlayer {
@@ -8,25 +8,70 @@ class MusicPlayer {
     this.btn = null;
     this.playlist = [];
     this.currentIndex = -1;
+    this.settings = this.getSettings();
     
     this.init();
+  }
+  
+  getSettings() {
+    try {
+      return JSON.parse(localStorage.getItem('shohada-settings')) || {
+        musicEnabled: true,
+        musicVolume: 50,
+        musicList: []
+      };
+    } catch(e) {
+      return { musicEnabled: true, musicVolume: 50, musicList: [] };
+    }
   }
   
   async init() {
     await this.loadPlaylist();
     this.createButton();
     
-    if (this.state.playing) {
+    this.applySettings();
+    
+    if (this.settings.musicEnabled && this.state.playing) {
       this.resumePlayback();
     }
   }
   
+  applySettings() {
+    if (this.audio) {
+      this.audio.volume = (this.settings.musicVolume || 50) / 100;
+      
+      if (this.settings.musicEnabled) {
+        if (this.audio.paused) {
+          this.audio.play().catch(() => {});
+        }
+      } else {
+        this.audio.pause();
+      }
+    }
+    
+    this.updateButton();
+  }
+  
   async loadPlaylist() {
     try {
-      const res = await fetch(`${MUSIC_API}?action=getMusicList`);
-      const data = await res.json();
-      if (data.success && data.data.length > 0) {
-        this.playlist = data.data.map(item => item.src);
+      const res = await fetch(`${MUSIC_API}?action=getMusic`);
+      const raw = await res.json();
+      
+      let data = [];
+      if (Array.isArray(raw)) data = raw;
+      else if (raw.success && Array.isArray(raw.data)) data = raw.data;
+      else if (raw.data && Array.isArray(raw.data)) data = raw.data;
+      
+      if (data.length > 0) {
+        const selectedList = this.settings.musicList || [];
+        
+        if (selectedList.length > 0) {
+          this.playlist = data
+            .map(item => typeof item === 'string' ? item : item.src)
+            .filter(src => selectedList.includes(src));
+        } else {
+          this.playlist = data.map(item => typeof item === 'string' ? item : item.src);
+        }
       }
     } catch(e) {}
   }
@@ -49,10 +94,13 @@ class MusicPlayer {
   }
   
   resumePlayback() {
-    if (!this.state.src) return;
+    if (!this.state.src) {
+      this.playRandom();
+      return;
+    }
     
     this.audio = new Audio(this.state.src);
-    this.audio.volume = 0.05;
+    this.audio.volume = (this.settings.musicVolume || 50) / 100;
     this.audio.currentTime = this.state.time || 0;
     
     this.audio.play().catch(() => {
@@ -73,8 +121,10 @@ class MusicPlayer {
       this.audio = null;
     }
     
+    if (!this.settings.musicEnabled) return;
+    
     this.audio = new Audio(src);
-    this.audio.volume = 0.05;
+    this.audio.volume = (this.settings.musicVolume || 50) / 100;
     this.state.src = src;
     this.state.playing = true;
     
@@ -106,6 +156,8 @@ class MusicPlayer {
   }
   
   toggle() {
+    this.settings = this.getSettings();
+    
     if (this.playlist.length === 0) {
       this.loadPlaylist().then(() => {
         if (this.playlist.length > 0) this.playRandom();
