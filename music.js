eab2ca50b83f0eb3e433e69967e53cc55590dd6d
panah <amirmahdi.code@gmail.com>
2026-09-07 22:52:48 +0330

    Update music.js

--- music.js
+++ music.js
@@ -9,7 +9,6 @@ class MusicPlayer {
     this.playlist = [];
     this.currentIndex = -1;
     this.settings = this.getSettings();
-    
     this.init();
   }
   
@@ -27,12 +26,13 @@ class MusicPlayer {
   
   async init() {
     await this.loadPlaylist();
-    this.createButton();
     
-    this.applySettings();
-    
-    if (this.settings.musicEnabled && this.state.playing) {
-      this.resumePlayback();
+    if (this.settings.musicEnabled) {
+      if (this.state.playing && this.state.src) {
+        this.resumePlayback();
+      } else if (this.playlist.length > 0) {
+        this.playRandom();
+      }
     }
   }
   
@@ -48,8 +48,6 @@ class MusicPlayer {
         this.audio.pause();
       }
     }
-    
-    this.updateButton();
   }
   
   async loadPlaylist() {
@@ -94,34 +92,25 @@ class MusicPlayer {
   }
   
   resumePlayback() {
-    if (!this.state.src) {
-      this.playRandom();
-      return;
-    }
+    if (!this.state.src) return;
     
     this.audio = new Audio(this.state.src);
     this.audio.volume = (this.settings.musicVolume || 50) / 100;
     this.audio.currentTime = this.state.time || 0;
     
-    this.audio.play().catch(() => {
-      this.state.playing = false;
-      this.saveState();
-      this.updateButton();
-    });
-    
-    this.updateButton();
+    this.audio.play().catch(() => {});
     
     this.audio.addEventListener('timeupdate', () => this.saveState());
     this.audio.addEventListener('ended', () => this.playNext());
   }
   
   play(src) {
+    if (!this.settings.musicEnabled) return;
+    
     if (this.audio) {
       this.audio.pause();
       this.audio = null;
     }
-    
-    if (!this.settings.musicEnabled) return;
     
     this.audio = new Audio(src);
     this.audio.volume = (this.settings.musicVolume || 50) / 100;
@@ -129,7 +118,6 @@ class MusicPlayer {
     this.state.playing = true;
     
     this.audio.play().catch(() => {});
-    this.updateButton();
     this.saveState();
     
     this.audio.addEventListener('timeupdate', () => this.saveState());
@@ -155,32 +143,6 @@ class MusicPlayer {
     this.play(this.playlist[this.currentIndex]);
   }
   
-  toggle() {
-    this.settings = this.getSettings();
-    
-    if (this.playlist.length === 0) {
-      this.loadPlaylist().then(() => {
-        if (this.playlist.length > 0) this.playRandom();
-      });
-      return;
-    }
-    
-    if (!this.audio || this.audio.paused) {
-      if (this.state.src) {
-        this.resumePlayback();
-        this.state.playing = true;
-        this.saveState();
-      } else {
-        this.playRandom();
-      }
-    } else {
-      this.audio.pause();
-      this.state.playing = false;
-      this.saveState();
-      this.updateButton();
-    }
-  }
-  
   stop() {
     if (this.audio) {
       this.audio.pause();
@@ -188,43 +150,14 @@ class MusicPlayer {
     }
     this.state.playing = false;
     this.saveState();
-    this.updateButton();
-  }
-  
-  createButton() {
-    if (document.getElementById('musicToggle')) return;
-    
-    const headerBtns = document.querySelector('.header-btns');
-    if (!headerBtns) return;
-    
-    const btn = document.createElement('button');
-    btn.className = 'theme-toggle';
-    btn.id = 'musicToggle';
-    btn.title = 'پخش مداحی';
-    btn.innerHTML = '<i class="fa-solid fa-music"></i>';
-    
-    btn.addEventListener('click', (e) => {
-      e.preventDefault();
-      this.toggle();
-    });
-    
-    headerBtns.insertBefore(btn, headerBtns.firstChild);
-    this.btn = btn;
-    this.updateButton();
-  }
-  
-  updateButton() {
-    if (!this.btn) return;
-    
-    const isPlaying = this.audio && !this.audio.paused;
-    if (isPlaying) {
-      this.btn.style.background = 'var(--primary)';
-      this.btn.style.color = '#fff';
-    } else {
-      this.btn.style.background = 'var(--bg2)';
-      this.btn.style.color = 'var(--text)';
-    }
   }
 }
 
 const musicPlayer = new MusicPlayer();
+
+window.addEventListener('storage', (e) => {
+  if (e.key === 'shohada-settings') {
+    musicPlayer.settings = musicPlayer.getSettings();
+    musicPlayer.applySettings();
+  }
+});
